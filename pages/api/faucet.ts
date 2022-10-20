c1dca8ef54faeb38d0e7f5c3783251454dc4ebab
Noy <jianyue.wang@p12.dev>
2022-10-20 17:13:02 +0800

    🐞 fix: return to exit

--- pages/api/faucet.ts
+++ pages/api/faucet.ts
@@ -9,7 +9,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { address, chainId, tokenAddr } = req.body;
 
     if (!address || !chainId || !tokenAddr) {
-      res.status(400).send({ error: 'Invalid request' });
+      res.status(400).json({ error: 'Invalid request' });
+      return;
     }
 
     const provider = new providers.JsonRpcProvider(RPC_URL[chainId]);
@@ -20,17 +21,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const p12Token = new ethers.Contract(P12_TOKEN_ADDRESS[chainId], erc20ABI, wallet);
         const tx = await p12Token.transfer(address, parseEther('100'));
 
-        res.status(200).send({ txHash: tx.hash });
+        res.status(200).json({ txHash: tx.hash });
+        return;
       } else if (tokenAddr === ethers.constants.AddressZero) {
         const tx = await wallet.sendTransaction({ to: address, value: parseEther('0.1') });
 
-        res.status(200).send({ txHash: tx.hash });
+        res.status(200).json({ txHash: tx.hash });
+        return;
       }
     } catch (e) {
-      res.status(500).send({ error: e });
+      res.status(500).json({ error: e });
+      return;
     }
-    res.status(400).send({ error: 'Invalid request' });
+    res.status(400).json({ error: 'Invalid request' });
+    return;
   } else {
-    res.status(400).send({ error: 'Invalid request' });
+    res.status(400).json({ error: 'Invalid request' });
+    return;
   }
 }
